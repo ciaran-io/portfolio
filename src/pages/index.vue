7b6ffe8f242b2232501742715d1dce6fd8646d3d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-01-10 00:28:35 +0000

    refactor: improve layout on small devices

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,15 @@
 <template>
-	<div>
-		<section
-			id="hero"
-			class="wrapper card h-96 gap-y-4 justify-items-center card-shadow grid w-full p-0 mt-24 text-center"
-		>
-			<div class="">
+	<div class="wrapper gutter-side md:mt-24 mt-12">
+		<section class="card card-shadow md:max-h-96 w-full text-center">
+			<div>
 				<div class="text-4xl text-white">Hi, welcome to my porfolio.</div>
 
-				<div class="text-gray-400 text-2xl w-[20ch] mt-8 mx-auto">
+				<div class="text-gray-400 text-2xl mx-auto max-w-[20ch] mt-8">
 					Thank you for taking the time to visit my site
 				</div>
 			</div>
 
-			<div class="text-base mx-auto w-[40ch] text-[#ec591f]">
+			<div class="text-base max-w-[40ch] text-[#ec591f] mt-12 mx-auto">
 				Hi, my name is Ciaran; I'm a frontend web developer from Ireland with a
 				passion to learn everything in the web technology space & more!
 
@@ -21,10 +18,8 @@
 			</div>
 		</section>
 
-		<main class="wrapper gutter-side mt-44">
-			<ContentJob />
+		<main class="mt-36">
+			<LazyContentJob />
 		</main>
 	</div>
-</template>
-
 </template>
